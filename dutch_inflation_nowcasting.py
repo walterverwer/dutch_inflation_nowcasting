ddd21512a79ae2dca0a50a332b971c9a0e0ce6d5
Walter Verwer <walterverwer@gmail.com>
2023-01-04 15:38:38 +0100

    test

--- dutch_inflation_nowcasting.py
+++ dutch_inflation_nowcasting.py
@@ -2,10 +2,46 @@ import os
 import pandas as pd
 import numpy as np
 
+from datetime import date
+from pandas.tseries.offsets import DateOffset
 
-# Set-up data
-DATA = 'dutch_inflation.csv'
+
+#%% - Data cleaning
+
+DATA = 'cpi_mm.csv'
 path = os.path.join('.', 'data', DATA)
-raw_data = pd.read_csv(path, sep=';')
+df = pd.read_csv(path, sep=';')
+
+# Setting the correct date
+'''
+For setting correct date:
+    De nieuwe cijfers worden doorgaans op de eerste donderdag van de maand gepubliceerd.
+    Tenzij de eerste donderdag op de eerste, tweede of derde dag van de maand valt of er
+    feestdagen vlak voor of op deze donderdag vallen. Dan worden de nieuwe cijfers de
+    dinsdag daarna gepubliceerd.
+    
+    To do:
+        1. Check what day of the month it is for every day in time.
+        2. Publication Thursday should be at least the 4th day of the month, 
+           which cannot be a holiday, e.g. 4th of may.
+        3. If Thursday is on the 1st, 2nd, 3th day or falls on a holiday,
+           then the publication day will be 5 days later, on a Tuesday 
+           (check whether this is true!). E.g. Thursday the 1st -> Tuesday the 6th.
+        4. For every date in time the above needs to hold.
+'''
+
+# Set perioden to time
+df['Time'] = df['Perioden'].str.replace('MM','-')
+
+# Time starting at the first of the month:
+df['Time'] = pd.to_datetime(df['Time'], format='%Y-%m')
+
+# Create temp day frame, which contains weekday number based on df['Time']
+df['Day'] = df['Time'].apply(date.isoweekday)
 
 
+'''
+- if thursday falls on any of the unallowed days:
+    df['Time'] + DateOffset(days=5), for df['Time'] which cannot be a
+    publication day.
+'''
---
+++ readme.txt
@@ -0,0 +1,24 @@
+Data from CBS:
+https://opendata.cbs.nl/statline/portal.html?_la=nl&_catalog=CBS&tableId=83131NED&_theme=378
+
+Data info:
+
+2006=100:
+- 011320 Diepvries vis
+- 031100 Kledingstoffen
+- 031420 Reparatie en verhuur van kleding
+- 032200 Reparatie en verhuur van schoenen
+- 043210 Diensten van loodgieters
+- 043230 Onderhoud van verwarming
+- 043250 Diensten van timmerlieden
+- 043290 Ov. onderhoudsdiensten woning
+- 051300 Reparatie van meubelen e.d.
+- 053190 Overige grote huishoudelijke apparaten
+- 063000 Diensten van ziekenhuizen
+- 091420 Onbespeelde informatiedragers
+- 094240 Huur van materiaal voor cultuur
+- 096010 Pakketreizen binnenland
+2000=100:
+- 134000 Onroerend zaakbelasting
+
+CPI: price index (2015=100)
